af75be2c220e9c5f299fdf83b433c6305fbc4088
José Augusto <joseaugusto0408@gmail.com>
2022-05-13 22:51:32 -0300

    trabalhando com mocks

---
+++ src/file.js
@@ -0,0 +1,67 @@
+const { readFile } = require('fs/promises');
+const User = require('./user');
+const { error } = require('./constants');
+
+const DEFAULT_OPTION = {
+  maxLines: 3,
+  fields: ["id", "name", "profession", "age"]
+}
+
+class File {
+  static async csvToJson(filePath) {
+    const content = await File.getFileContent(filePath);
+    const validation = File.isValid(content);
+    if (!validation.valid) throw new Error(validation.error)
+
+    const users = File.parseCSVToJSON(content);
+
+    return users;
+  }
+
+  static async getFileContent(filePath) {
+    return (await readFile(filePath)).toString('utf-8');
+  }
+
+  static isValid(csvString, options = DEFAULT_OPTION) {
+    //dividindo o arquivo por linha
+    const [header, ...fileWithoutHeader] = csvString.trim().split('\n');
+
+    const isHeaderValid = header === options.fields.join(',');
+    if (!isHeaderValid) {
+      return {
+        error: error.FILE_FIELDS_ERROR_MESSAGE,
+        valid: false
+      }
+    }
+    const isContentLengthAccepted = (
+      fileWithoutHeader.length > 0 &&
+      fileWithoutHeader.length <= options.maxLines
+    )
+    if (!isContentLengthAccepted) {
+
+      return {
+        error: error.FILE_LENGTH_ERROR_MESSAGE,
+        valid: false
+      }
+    }
+
+    return { valid: true }
+  }
+  static parseCSVToJSON(csvString) {
+    const lines = csvString.trim().split('\n');
+    //remove o primeio item e joga na variavel
+    const firstLine = lines.shift();
+    const header = firstLine.split(',');
+    const users = lines.map(line => {
+      const columns = line.split(',')
+      let user = {}
+      for (const index in columns) {
+        user[header[index]] = columns[index]
+      }
+      return new User(user);
+    })
+    return users;
+  }
+}
+
+module.exports = File;
